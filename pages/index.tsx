ec7eacefc1ab0985aed3234535d04dcf01b0a335
Omer Geva <ogeva97@gmail.com>
2022-08-01 19:21:04 +0300

    testing: video player

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import EmailForm from '../components/email-form/email-form.component';
 import withLayout from '../hocs/withLayout';
 
 import styles from '../styles/Home.module.scss'
+import ReactPlayer from 'react-player'
 
 // Components
 
 const Home: React.FC = ({}) => {
   return (
     <div className={styles.homeContainer}>
+      <div className={styles.videoContainer} >
+        <ReactPlayer loop={true} playing={true} controls={true} url='https://res.cloudinary.com/daf8abic5/video/upload/v1659369952/home-video_l5cqny.mp4' />
+      </div>
       <div className={styles.emailFormContainer}>
-        <h1>Semi Aquatics</h1>
         <EmailForm />
       </div>
     </div>
@@ -18,9 +21,3 @@ const Home: React.FC = ({}) => {
 
 export default withLayout(Home);
 
-{/* <div className={styles.videoContainer} dangerouslySetInnerHTML={{
-  __html: `
-  <video style="width: 100%; height: 100%; object-fit: cover;" loop autoplay>
-    <source src="/home-video.mp4" type="video/mp4"/>
-  </video>
-` }}></div> */}
