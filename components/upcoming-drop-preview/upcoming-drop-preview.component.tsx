a9200d0cb88fa97f1e3d57a07e713974313033b7
Omer Geva <ogeva97@gmail.com>
2022-08-07 21:11:08 +0300

    feat: remove countdown from drop preview

--- components/upcoming-drop-preview/upcoming-drop-preview.component.tsx
+++ components/upcoming-drop-preview/upcoming-drop-preview.component.tsx
@@ -1,23 +1,21 @@
 import Link from 'next/link';
 import { UpcomingDropPreviewProps } from '../../interfaces/page_interface';
-import CountdownTimer from '../countdown-timer/countdown-timer.component';
 import styles from './UpcomingDropPreview.module.scss'
 
 const UpcomingDropPreview: React.FC<UpcomingDropPreviewProps> = ({ timeLeftObj, products, dropTitle }) => (
   <div className={styles.upcomingDropPreviewContainer}>
     <div className={styles.productsPreviewContainer}>
-      {products.map((product: any) => (
+      {/* {products.map((product: any) => (
         <Link href={`drop/${product.node.id}`}>
           <div key={product.node.id} className={styles.productPreviewImage}>
             <img src={product.node.images.edges[0].node.transformedSrc} alt={product.node.images.edges[0].node.transformedSrc} />
           </div>
         </Link>
-      ))}
+      ))} */}
 
     </div>
     <div className={styles.titleAndTimer}>
       <h1>{dropTitle}</h1>
-      <CountdownTimer timeLeftObj={timeLeftObj} />
     </div>
   </div>
 );
