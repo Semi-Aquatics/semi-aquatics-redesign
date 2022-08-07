03046ba66cd62778f9a8efb24309ccd31ba94c6a
Omer Geva <ogeva97@gmail.com>
2022-08-07 22:58:17 +0300

    video integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,34 @@
-import EmailForm from '../components/email-form/email-form.component';
-import withLayout from '../hocs/withLayout';
+import EmailForm from "../components/email-form/email-form.component";
+import withLayout from "../hocs/withLayout";
 
-import styles from '../styles/Home.module.scss'
-import ReactPlayer from 'react-player'
-import SpinningLogo from '../components/spinning-logo/spinning-logo.component';
+import styles from "../styles/Home.module.scss";
+// import ReactPlayer from "react-player";
+// import SpinningLogo from "../components/spinning-logo/spinning-logo.component";
 
 // Components
 
-const Home: React.FC = ({}) => {
+const Home: React.FC = ({ }) => {
   return (
     <div className={styles.homeContainer}>
-      {/* <div className={styles.videoContainer} >
-        <ReactPlayer loop={true} playing={true} controls={true} url='https://res.cloudinary.com/daf8abic5/video/upload/v1659369952/home-video_l5cqny.mp4' />
-      </div> */}
-      <h1>Semi Aquatics</h1>
-      <div className={styles.spinningLogoContainer}>
-        <SpinningLogo />
+      <div className={styles.videoContainer}>
+        <video autoPlay muted loop className={styles.mainVideo}>
+          <source
+            src="https://res.cloudinary.com/daf8abic5/video/upload/v1659369952/home-video_l5cqny.mp4"
+            type="video/mp4"
+          />
+        </video>
       </div>
-      <div className={styles.emailFormContainer}>
-        <EmailForm />
+      <div className={styles.mainContainer}>
+        <h1>Semi Aquatics</h1>
+        {/* <div className={styles.spinningLogoContainer}>
+        <SpinningLogo />
+      </div> */}
+        <div className={styles.emailFormContainer}>
+          <EmailForm />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default withLayout(Home);
-
