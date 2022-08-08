50f1b349fd486ef5417d3ef96b8601f603257af5
Omer Geva <ogeva97@gmail.com>
2022-08-08 19:57:58 +0300

    fix: changed video source

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,7 @@ import EmailForm from "../components/email-form/email-form.component";
 import withLayout from "../hocs/withLayout";
 
 import styles from "../styles/Home.module.scss";
-// import ReactPlayer from "react-player";
-// import SpinningLogo from "../components/spinning-logo/spinning-logo.component";
 
-// Components
 
 const Home: React.FC = ({ }) => {
   return (
@@ -13,16 +10,13 @@ const Home: React.FC = ({ }) => {
       <div className={styles.videoContainer}>
         <video autoPlay muted loop className={styles.mainVideo}>
           <source
-            src="https://res.cloudinary.com/daf8abic5/video/upload/v1659369952/home-video_l5cqny.mp4"
+            src="https://res.cloudinary.com/daf8abic5/video/upload/v1659977833/video-semi-aquatics_oqkd2d.mp4"
             type="video/mp4"
           />
         </video>
       </div>
       <div className={styles.mainContainer}>
-        <h1>Semi Aquatics</h1>
-        {/* <div className={styles.spinningLogoContainer}>
-        <SpinningLogo />
-      </div> */}
+
         <div className={styles.emailFormContainer}>
           <EmailForm />
         </div>
