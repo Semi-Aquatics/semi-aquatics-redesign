c335f375035fddcf3181ef453503c4532056f5a0
Omer Geva <ogeva97@gmail.com>
2022-11-24 17:53:55 +0200

    temp: timer change

--- components/countdown-timer/countdown-timer.component.tsx
+++ components/countdown-timer/countdown-timer.component.tsx
@@ -4,7 +4,7 @@ import { calculateTimeLeft } from '../../utils/calculate_time_left';
 import styles from './CountdownTimer.module.scss'
 
 const CountdownTimer: React.FC = () => {
-  const DROP_DATE = new Date("2022/11/28 17:00:00 EST");
+  const DROP_DATE = new Date("2022/11/28 18:00:00 EST");
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(DROP_DATE));
   const totalTimeLeft: number = Object.values(timeLeft).reduce((a: any, b: any) => a + b);
   if (totalTimeLeft > 0) {
