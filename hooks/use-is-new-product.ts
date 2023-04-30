a3717ad1083d85d6d608c7f9ff38e94a7718b64b
Omer Geva <ogeva97@gmail.com>
2023-04-30 07:28:26 +0300

    sweatpants upcoming drop

--- hooks/use-is-new-product.ts
+++ hooks/use-is-new-product.ts
@@ -4,6 +4,7 @@ const NEW_PRODUCT_IDS: string[] = [
     "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzY4MjY1NTYyOTMxOTU=",
     "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzY4MjY1NTYzMjU5NjM=",
     "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzY4MjY1NTYyNjA0Mjc=",
+    "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzY4MjY1NTYyMjc2NTk=",
 ]
 
 export const useIsNewProduct = (id: string) => {
