d36d6c15776da4c84176f1e4545dca73a36b7814
Omer Geva <ogeva97@gmail.com>
2025-01-14 19:36:20 +0200

    feat: initial changes

--- components/product-preview/product-preview.component.tsx
+++ components/product-preview/product-preview.component.tsx
@@ -1,35 +1,48 @@
+import React from 'react';
 import { useIsNewProduct } from '../../hooks/use-is-new-product';
-import styles from './ProductPreview.module.scss'
-import Link from 'next/link'
+import styles from './ProductPreview.module.scss';
+import Link from 'next/link';
 
 interface ProductPreviewProps {
-    image: string,
-    title: string,
-    id: string,
-    isSoldOut: boolean,
-    isArchive: boolean | undefined,
-    isTimeLeft?: boolean
+  image: string;
+  title: string;
+  id: string;
+  isSoldOut: boolean;
+  isArchive: boolean;
+  price?: string;
+  isTimeLeft?: boolean;
 }
 
-const ProductPreview:React.FC<ProductPreviewProps> = ({image, title, id, isArchive, isSoldOut, isTimeLeft }) => {
-  const isNewProduct = useIsNewProduct(id)
-  const deconstructedId = id.split('/').splice(-1)[0]
+const ProductPreview: React.FC<ProductPreviewProps> = ({
+  image,
+  title,
+  id,
+  isSoldOut,
+  isArchive,
+  price,
+  isTimeLeft,
+}) => {
+  const isNewProduct = useIsNewProduct(id);
+  const deconstructedId = id.split('/').splice(-1)[0];
 
   return (
     <div className={styles.productPreviewContainer}>
       <Link href={`drop/${deconstructedId}`}>
         <div>
-          {
-            isSoldOut &&
+          {isSoldOut && (
             <div className={styles.soldOut}>
-                  <h3>{ isNewProduct && isTimeLeft ? 'COMING SOON' : 'SOLD OUT'}</h3>
-              </div>
-          }
-          <img src={image} alt={title}/>
-          {
-            !isArchive &&
-            <h3 className={styles.cardTitle}>{title}</h3>
-          }
+              <h3>{isNewProduct && isTimeLeft ? 'COMING SOON' : 'SOLD OUT'}</h3>
+            </div>
+          )}
+          <div className={styles.imageContainer}>
+            <img src={image} alt={title} />
+          </div>
+          {!isArchive && (
+            <div className={styles.previewDetails}>
+              <h3 className={styles.cardDetail}>{title}</h3>
+              <h3 className={styles.cardDetail}>${price}0</h3>
+            </div>
+          )}
         </div>
       </Link>
     </div>
